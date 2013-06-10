4e57caa66b5cafeaa1b8f87a7887f1443a073265
Athos Ribeiro <athosxd@gmail.com>
2013-06-10 12:54:41 -0300

    Screen resolution set

--- include/game.h
+++ include/game.h
@@ -11,8 +11,8 @@ class Game {
         void shutdown();
 
     private:
-        static const int SCREEN_WIDTH = 800;
-        static const int SCREEN_HEIGHT = 600;
+        static const int SCREEN_WIDTH = 854;
+        static const int SCREEN_HEIGHT = 480;
         static const int SCREEN_BPP = 32;
 
         SDL_Surface *screen;
