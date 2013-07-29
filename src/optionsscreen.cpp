9a2c91bd105c24adc029e333e9b25394719549c6
Mateus Furquim <furquim.axs@gmail.com>
2013-07-29 05:05:33 -0300

    Adding Help on Options Menu

--- src/optionsscreen.cpp
+++ src/optionsscreen.cpp
@@ -6,19 +6,24 @@ using namespace std;
 OptionsScreen::OptionsScreen(string filename)
 {
 	this->optionsScreen = SDLUtil::loadImage(filename.c_str());
+	this-> helpMessage = SDLUtil::loadImage("resources/helpTextImage.png");
 }
 
 OptionsScreen::~OptionsScreen()
 {
-	if (optionsScreen != NULL)
+	if (this->optionsScreen != NULL)
 	{
-		SDL_FreeSurface(optionsScreen);
+		SDL_FreeSurface(this->optionsScreen);
+	}
+	if (this->helpMessage != NULL)
+	{
+		SDL_FreeSurface(this->helpMessage);
 	}
 }
 
 void OptionsScreen::drawSelf(SDL_Surface *surface)
 {
 	SDLUtil::applySurface(0, 0, this->optionsScreen, surface);
-
+	SDLUtil::applySurface(20, 100, this->helpMessage, surface);
 	return ;
 }
